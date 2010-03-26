513427b6c10341728d004b0aad010335106375b7
Matt Robinson <git@nerdoftheherd.com>
2010-03-26 18:31:29 +0000

    Added a copyright header to the source files.

--- BbcScreen.h
+++ BbcScreen.h
@@ -1,3 +1,19 @@
+/**************************************************************************************************************
+  Utility to display BBC Micro / Master Graphics Files.
+  Copyright © 2007-2010 Matt Robinson, © 2003 David Robinson
+ 
+  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+ 
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+ 
+  You should have received a copy of the GNU General Public License along with this program; if not, write
+  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**************************************************************************************************************/
+
 #pragma once
 
 #define BBC_WIDTH0   640   // width of BBC MODE0 screen
