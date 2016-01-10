5d36091b4aae7707f6928e61ecf8ba7cfcf9c37e
Matt Robinson <git@nerdoftheherd.com>
2016-01-10 17:30:16 +0000

    Re-license as GPL 3 and update source file headers

--- BbcScreen.cpp
+++ BbcScreen.cpp
@@ -1,18 +1,20 @@
-/**************************************************************************************************************
-  Utility to display BBC Micro / Master Graphics Files.
-  Copyright © 2007-2010 Matt Robinson, © 2003 David Robinson
- 
-  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
-  Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
-  option) any later version.
- 
-  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-  License for more details.
- 
-  You should have received a copy of the GNU General Public License along with this program; if not, write
-  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-**************************************************************************************************************/
+/*
+ * This file is part of BBC Graphics Viewer.
+ * Copyright Â© 2003-2010 by the authors - see the AUTHORS file for details.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "stdafx.h"
 #include "BbcScreen.h"
